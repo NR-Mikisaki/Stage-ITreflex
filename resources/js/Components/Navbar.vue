586cd45ad71e3398df2525a43598337e43d8fb58
Nicolas Robinet <nicolas.robinet@student.coltd.be>
2024-03-22 10:05:06 +0100

    search popup and products table

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -193,15 +193,49 @@
                                     <span class="sr-only">, change currency</span>
                                 </a>
                             </div>
-
-                            <!-- Search -->
                             <div class="flex lg:ml-6">
                                 <a href="#" class="p-2 text-gray-400 hover:text-gray-500">
-                                    <span class="sr-only">Search</span>
-                                    <SearchIcon class="h-6 w-6" aria-hidden="true" />
+                                    <button @click="searchbarOpen = true">
+                                        <span class="sr-only">Search</span>
+                                        <SearchIcon class="h-6 w-6" aria-hidden="true" />
+                                    </button>
                                 </a>
-                            </div>
+                        </div>
 
+                            <!-- Search Popup -->
+                            <TransitionRoot as="template" :show="searchbarOpen">
+                                <Dialog as="div" class="relative z-10" @close="searchbarOpen = false">
+                                    <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+                                        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                                    </TransitionChild>
+
+                                    <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                                        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                            <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+                                                <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                                    <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                                        <div class="sm:flex sm:items-start">
+                                                            <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                                                <search-icon class="h-6 w-6 text-red-600" aria-hidden="true" />
+                                                            </div>
+                                                            <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                                                <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">Searchbar</DialogTitle>
+                                                                <div class="mt-2">
+                                                                    <p class="text-sm text-gray-500">What do you want to search?</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                                        <button type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto" @click="open = false">Search</button>
+                                                        <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" @click="open = false" ref="cancelButtonRef">Cancel</button>
+                                                    </div>
+                                                </DialogPanel>
+                                            </TransitionChild>
+                                        </div>
+                                    </div>
+                                </Dialog>
+                            </TransitionRoot>
                             <!-- Cart -->
                             <popover-group>
                                 <popover class="ml-4 flow-root lg:ml-6" :show="true">
@@ -252,6 +286,7 @@ import {ChevronDownIcon} from "@heroicons/vue/solid";
 
 const open = ref(false)
 const isLoggedIn = ref(false)
+const searchbarOpen = ref(false)
 
 const openRegistrationModal = () => {
     window.location.href='/registration-page'
@@ -270,14 +305,10 @@ const navigation = {
 export default {
     data() {
         return {
-            categoriesOpen: false
+            categoriesOpen: false,
         };
     },
-    methods: {
-        toggleCategoriesPanel() {
-            this.categoriesOpen = !this.categoriesOpen;
-        }
-    }
+
 };
 </script>
 
